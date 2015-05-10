4a8034871f0b28f787f501f465604dc8f1d75013
Esther Gutiérrez <im@esthergutierrez.info>
2015-05-10 12:44:31 +0200

    Debug

--- www/myService.js
+++ www/myService.js
@@ -1,3 +1,2 @@
-var serviceName = 'com.kesthers.plugins.bgs.MyService';
 var factory = require('com.kesthers.plugins.bgs.BackgroundService');
-module.exports = factory.create(serviceName);
+module.exports = factory.create('com.kesthers.plugins.bgs.MyService');
